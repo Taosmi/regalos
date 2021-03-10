d4d7d2afc6f38c56fc9ee13fba199c9e19fbac8a
Zeta Uppercase <magarcia@taosmi.es>
2021-03-10 17:15:23 +0100

    Vendor dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dca294686bba3c5fd28a930b7ab8b50335532424',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dca294686bba3c5fd28a930b7ab8b50335532424',
+    ),
+    'gettext/gettext' => 
+    array (
+      'pretty_version' => 'v5.5.4',
+      'version' => '5.5.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '77baf40326b4eac0230cd11ee75680969dcbe72a',
+    ),
+    'gettext/languages' => 
+    array (
+      'pretty_version' => '2.6.0',
+      'version' => '2.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38ea0482f649e0802e475f0ed19fa993bcb7a618',
+    ),
+  ),
+);
