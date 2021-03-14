fc7b8493060a7317633425fdbbca423afcaaaca0
Zeta Uppercase <magarcia@taosmi.es>
2021-03-14 18:14:09 +0100

    testing

--- runPHP/ApiController.php
+++ runPHP/ApiController.php
@@ -58,13 +58,13 @@ abstract class ApiController {
         $this->input = $_REQUEST;
         // Get the body request input data.
         if ($request['mime'] === 'application/json' && $request['method'] != 'GET') {
-            echo(json_decode(file_get_contents('php://input'), true));
+            print_r(json_decode(file_get_contents('php://input'), true));
             $this->input = array_merge($this->input, json_decode(file_get_contents('php://input'), true));
             // parse_str(file_get_contents('php://input'), $this->input);
         // } else {
             // $this->input = $_REQUEST;
         }
-        echo($this->input);
+        print_r($this->input);
     }
 
 
