cf3c6e944bfef4b711aafa8ceeed60c388cd178e
Zeta Uppercase <magarcia@taosmi.es>
2021-03-27 18:32:24 +0100

    Eliminate test prints

--- webapp/apis/v1/user.php
+++ webapp/apis/v1/user.php
@@ -158,7 +158,6 @@ class user extends ApiController {
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
         // Execute post.
         $result = json_decode(curl_exec($ch), true);
-var_dump($result);
         return $result['data']['url'];
     }
 
